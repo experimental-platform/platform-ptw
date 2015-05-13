fcb0e29dc5fe56228bc52e0e0d99eb9f53b1544f
Andreas Litt <andreas.litt@gmail.com>
2015-05-13 12:12:01 +0200

    read nodename from file

--- Dockerfile
+++ Dockerfile
@@ -17,7 +17,7 @@ COPY publish_to_web-1.1.0.gem /tmp/
 # Install gem
 RUN gem install /tmp/publish_to_web-1.1.0.gem
 
-ENV PROTONET_CONFIG /data
+ENV PROTONET_CONFIG /config
 COPY start_publish_to_web.rb /bin/start_publish_to_web
 RUN chmod 755 /bin/start_publish_to_web
 
--- start_publish_to_web.rb
+++ start_publish_to_web.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
 PORT = 80
-unless ENV["BINDHOST_PORT_#{PORT}_TCP_ADDR"] || ENV["HOSTNAME"]
-  puts "$BINDHOST_PORT_80_TCP_ADDR or $HOSTNAME is not set!"
-  puts "Please start this container with linked bindhost container, exposed port #{PORT} and defined hostname."
-  puts "eg. -> docker run -d --name publish_to_web --hostname foobar.protonet.info --link <to bind container>:bindhost protonet/publish_to_web"
+unless ENV["BINDHOST_PORT_#{PORT}_TCP_ADDR"]
+  puts "$BINDHOST_PORT_80_TCP_ADDR is not set!"
+  puts "Please start this container with linked bindhost container, exposed port #{PORT}."
+  puts "eg. -> docker run -d --volume=/etc/protonet/ptw:/config --name publish_to_web --link <to bind container>:bindhost protonet/publish_to_web"
   exit 127
 end
 
@@ -12,6 +12,7 @@ require 'publish_to_web'
 
 CONFIG_PATH = ENV["PROTONET_CONFIG"] || "/tmp"
 IDENTIFIER_FILE = "#{CONFIG_PATH}/hardware_id"
+NODENAME_FILE = "#{CONFIG_PATH}/nodename"
 
 FileUtils.mkdir_p(CONFIG_PATH)
 
@@ -26,12 +27,15 @@ def generate_hardware_id
   "aal-#{SecureRandom.uuid}"
 end
 
+def expected_nodename
+  IO.read(NODENAME_FILE).strip rescue ""
+end
+
 ensure_identifier_exists
 
 publish_to_web = PublishToWeb.new
 _, nodename, _ = publish_to_web.info
 
-expected_nodename = ENV["HOSTNAME"]
 if !expected_nodename.empty? && nodename != expected_nodename
   publish_to_web.set_node_name expected_nodename.gsub(/\.protonet\.info$/, "") #this is ugly
 end
